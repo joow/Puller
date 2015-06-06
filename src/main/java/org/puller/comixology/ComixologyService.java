3322308ef95229a5faf28a5f5ec5051e7c82d2ed
Benoît Giraudou <bgiraudou@gmail.com>
2015-06-06 16:24:48 +0200

    Add required methods

--- src/main/java/org/puller/comixology/ComixologyService.java
+++ src/main/java/org/puller/comixology/ComixologyService.java
@@ -1,8 +1,16 @@
 package org.puller.comixology;
 
-import retrofit.http.GET;
+import retrofit.http.*;
 
 public interface ComixologyService {
     @GET("/")
     String getIndex();
+
+    @FormUrlEncoded
+    @POST("/login")
+    String login(@Field("username") String username,
+                 @Field("password") String password, @Field("PL_CSRF_TOKEN") String csrfToken);
+
+    @GET("/printlist/{year}/{month}/{day}")
+    String getWeeklyPullList(@Path("year") String year, @Path("month") String month, @Path("day") String day);
 }

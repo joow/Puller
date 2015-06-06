6e40a8a2f612856943e80cee82ce5fb19a3852ad
Benoît Giraudou <bgiraudou@gmail.com>
2015-06-06 16:32:14 +0200

    Use @FieldMap annotation for user authentication

--- src/main/java/org/puller/comixology/ComixologyService.java
+++ src/main/java/org/puller/comixology/ComixologyService.java
@@ -2,14 +2,15 @@ package org.puller.comixology;
 
 import retrofit.http.*;
 
+import java.util.Map;
+
 public interface ComixologyService {
     @GET("/")
     String getIndex();
 
     @FormUrlEncoded
     @POST("/login")
-    String login(@Field("username") String username,
-                 @Field("password") String password, @Field("PL_CSRF_TOKEN") String csrfToken);
+    String login(@FieldMap Map<String, String> user, @Field("PL_CSRF_TOKEN") String csrfToken);
 
     @GET("/printlist/{year}/{month}/{day}")
     String getWeeklyPullList(@Path("year") String year, @Path("month") String month, @Path("day") String day);
---
+++ src/main/java/org/puller/comixology/ComixologyUser.java
@@ -0,0 +1,25 @@
+package org.puller.comixology;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ComixologyUser {
+    private static final String USERNAME_FIELD = "username";
+    private static final String PASSWORD_FIELD = "password";
+
+    private final String username;
+    private final String password;
+
+    public ComixologyUser(final String username, final String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Map<String, String> toMap() {
+        final HashMap<String, String> map = new HashMap<>();
+        map.put(USERNAME_FIELD, username);
+        map.put(PASSWORD_FIELD, password);
+
+        return map;
+    }
+}
